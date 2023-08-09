f92b0525c4acd25bf2b06dc4c2709250a91a05ee
Dennis <denjp17@gmail.com>
2023-08-09 15:31:27 -0600

    Added birdImage LWC Updated app styling

--- force-app/main/default/lwc/eBirdRecentObservationsSpecies/eBirdRecentObservationsSpecies.js
+++ force-app/main/default/lwc/eBirdRecentObservationsSpecies/eBirdRecentObservationsSpecies.js
@@ -2,11 +2,14 @@ import { LightningElement, api, wire, track } from 'lwc';
 import { getRecord } from 'lightning/uiRecordApi';
 import getRecentObservations from '@salesforce/apex/eBirdRecentObservationsSpecies.getRecentObservations';
 
+
+
 export default class RecentObservations extends LightningElement {
     @api recordId; // Bird record Id passed to the component
     @track speciesCode;
     @track birdName;
     @track birdData;
+
 
     @wire(getRecord, { recordId: '$recordId', fields: ['Bird__c.Name', 'Bird__c.eBird_Species_Code__c'] })
     birdRecord({ error, data }) {
@@ -29,10 +32,12 @@ export default class RecentObservations extends LightningElement {
         // Call the Apex method to fetch recent observations
         getRecentObservations({ speciesCode: speciesCode })
             .then(result => {
-                this.birdData = JSON.parse(result);
-                console.log(this.birdData);
-                console.log(speciesCode);
-            })
+                this.birdData = JSON.parse(result).map(observation => {
+            // Format the observation date using the user's locale
+                observation.obsDt = new Date(observation.obsDt).toLocaleDateString();
+                return observation;
+            });
+         })
             .catch(error => {
                 console.log('Error fetching eBird observations:', error);
             });

85456151d1591fe2d05c21665a98e31e7003dee1
Dennis <denjp17@gmail.com>
2023-08-26 17:21:57 -0600

    Added Comments and cleaned up commented code

--- force-app/main/default/lwc/birdImageUpload/birdImageUpload.js
+++ force-app/main/default/lwc/birdImageUpload/birdImageUpload.js
@@ -1,77 +1,55 @@
-import { LightningElement, api, wire } from 'lwc';
-import { ShowToastEvent } from 'lightning/platformShowToastEvent';
-import saveAttachment from '@salesforce/apex/BirdController.saveAttachment';
-import getAttachment from '@salesforce/apex/BirdController.getAttachment';
-import getBird from '@salesforce/apex/BirdController.getBird';
+import { LightningElement, wire, api } from 'lwc'
+import { ShowToastEvent } from 'lightning/platformShowToastEvent'
+import getAttachment from '@salesforce/apex/BirdController.getAttachment'
+import getBird from '@salesforce/apex/BirdController.getBird'
+import { refreshApex } from '@salesforce/apex'
 
 export default class BirdImageUpload extends LightningElement {
-    @api recordId;
-    imageUrl;
-    file;
-    uploadDisabled = true;
-    birdName;
+  @api recordId
+  imageUrl
+  file
+  birdName
+  wiredBirdImage
 
-    @wire(getBird, { birdId: '$recordId' })
-    wiredBird({ error, data }) {
-        if (data) {
-            this.birdName = data.Name;
-        } else if (error) {
-            console.error(error);
-        }
+  // file formats for lightning-file-upload
+  get acceptedFormats() {
+    return ['.jpg', '.jpeg', '.png']
+  }
+
+  // get record name to display
+  @wire(getBird, { birdId: '$recordId' })
+  wiredBird({ error, data }) {
+    if (data) {
+      this.birdName = data.Name
+      console.log(this.recordId)
+    } else if (error) {
+      console.error(error)
     }
+  }
 
-    connectedCallback() {
-        getAttachment({ parentId: this.recordId })
-            .then(result => {
-                if (result && result.Id) {
-                    this.imageUrl = '/servlet/servlet.FileDownload?file=' + result.Id;
-                }
-            })
-            .catch(error => {
-                console.error(error);
-            });
+  // checks to see if there is an image already attached to the record and sets it to the image url
+  @wire(getAttachment, { parentId: '$recordId' })
+  wiredAttachment(wireResult) {
+    const { data, error } = wireResult
+    this.wiredBirdImage = wireResult
+    if (data) {
+      this.imageUrl = '/sfc/servlet.shepherd/version/download/' + data
+      console.log('this.imageUrl: ' + this.imageUrl)
+    } else if (error) {
+      console.error(error)
     }
+  }
 
-    handleFileChange(event) {
-        this.file = event.target.files[0];
-        this.imageUrl = URL.createObjectURL(this.file);
-        this.uploadDisabled = false;
-    }
+  // fires when file is uploaded
+  handleUploadFinished(event) {
+    const uploadedFiles = event.detail.files
+    console.log('uploadedFiles: ' + JSON.stringify(uploadedFiles))
+    const successMessage = new ShowToastEvent({
+      title: 'Success!',
+      message: 'Bird image uploaded successfully.',
+    })
+    this.dispatchEvent(successMessage)
 
-    handleUpload() {
-        const reader = new FileReader();
-        reader.onload = () => {
-            const fileContents = reader.result;
-            const base64 = 'base64,';
-            const dataStart = fileContents.indexOf(base64) + base64.length;
-            const fileData = fileContents.substring(dataStart);
-            const fileName = this.file.name;
-            const contentType = this.file.type;
-
-            saveAttachment({ parentId: this.recordId, fileName: fileName, contentType: contentType, base64Data: fileData })
-                .then(() => {
-                    this.dispatchEvent(
-                        new ShowToastEvent({
-                            title: 'Success',
-                            message: 'Attachment uploaded successfully',
-                            variant: 'success'
-                        })
-                    );
-                    this.file = null;
-                    this.imageUrl = null;
-                    this.uploadDisabled = true;
-                })
-                .catch(error => {
-                    console.error(error);
-                    this.dispatchEvent(
-                        new ShowToastEvent({
-                            title: 'Error',
-                            message: 'Error uploading attachment',
-                            variant: 'error'
-                        })
-                    );
-                });
-        };
-        reader.readAsDataURL(this.file);
-    }
+    return refreshApex(this.wiredBirdImage)
+  }
 }
